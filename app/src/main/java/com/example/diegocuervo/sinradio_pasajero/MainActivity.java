8ebed5788d5e3e23ace2350eb323f20d76ff5ce0
Diego Cuervo <diegonoya93@hotmail.com>
2016-09-16 18:20:38 -0300

    setting

--- app/src/main/java/com/example/diegocuervo/sinradio_pasajero/MainActivity.java
+++ app/src/main/java/com/example/diegocuervo/sinradio_pasajero/MainActivity.java
@@ -99,7 +99,9 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-
+        if (id == R.id.action_settings) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
