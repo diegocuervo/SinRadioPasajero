e4d00bfb739b4e5d89b5821b34d36dca3aa42123
Diego Cuervo <diegonoya93@hotmail.com>
2016-10-09 02:56:26 -0300

    maps con posicion andando

--- app/src/main/java/com/example/diegocuervo/sinradio_pasajero/Pedir_taxi.java
+++ app/src/main/java/com/example/diegocuervo/sinradio_pasajero/Pedir_taxi.java
@@ -1,16 +1,25 @@
 package com.example.diegocuervo.sinradio_pasajero;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -24,7 +33,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Diego Cuervo on 08/10/16.
@@ -32,10 +46,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Pedir_taxi extends Fragment implements OnMapReadyCallback {
     MapView mMapView;
     private GoogleMap googleMap;
-
+    private Marker marker;
     public Pedir_taxi() {
         // Required empty public constructor
     }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.pedir_taxi, container, false);
 
@@ -54,22 +69,47 @@ public class Pedir_taxi extends Fragment implements OnMapReadyCallback {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
+                    @Override
+                    public void onMapClick(LatLng arg0) {
+                        Geocoder geocoder;
+                        List<Address> addresses;
+                        geocoder = new Geocoder(getActivity(), Locale.getDefault());
+
+                        try {
+                            addresses = geocoder.getFromLocation(arg0.latitude, arg0.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+
+                        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                        String city = addresses.get(0).getLocality();
+                        String state = addresses.get(0).getAdminArea();
+                        String country = addresses.get(0).getCountryName();
+                        String postalCode = addresses.get(0).getPostalCode();
+                        String knownName = addresses.get(0).getFeatureName(); // On
+                            Log.w("direccion",address);
+                            MarkerOptions markerOptions = new MarkerOptions();
+                               markerOptions.position(arg0);
+                            markerOptions.title(address);
+                            googleMap.clear();
+                             googleMap.animateCamera(CameraUpdateFactory.newLatLng(arg0));
+                              googleMap.addMarker(markerOptions);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 // For showing a move to my location button
                 googleMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setZoomControlsEnabled(true);
 
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
 
         return rootView;
     }
+
+
 
     @Override
     public void onResume() {
